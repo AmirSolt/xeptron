54c0e8e3ba7ab31852c818d49c4d491541596808
AmirSolt <soltamir194@gmail.com>
2023-09-23 05:51:10 -0400

    upd

--- src/lib/comp/aiForms/AIAnswer.svelte
+++ src/lib/comp/aiForms/AIAnswer.svelte
@@ -12,13 +12,13 @@
     
 <div class="card variant-ringed flex flex-col justify-center items-start" >
 
-    <div class="flex justify-between items-center w-full">
+    <div class="flex flex-col justify-start items-start md:flex-row md:justify-between md:items-center gap-4 md:gap-0 w-full">
         <h1>
             Output
         </h1>
         
 
-        <div class="flex justify-end items-center w-full gap-4">
+        <div class="flex justify-between md:justify-end items-center w-full gap-4">
             <CopyClipboard text={response} extraStyle={isStreamingOver? 'variant-filled-primary':'variant-ringed pointer-events-none opacity-50'} />
             
             <span class="text-xl badge p-3 variant-soft">
